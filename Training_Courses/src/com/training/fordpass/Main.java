622d42156c8011d68f4986865f5c9016eee6573f
ScottDonovan <spdonovan@hotmail.co.uk>
2022-01-18 12:24:38 +0000

    using operator precedence

--- Training_Courses/src/com/training/fordpass/Main.java
+++ Training_Courses/src/com/training/fordpass/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
        System.out.println("We got started!!");
+       int valA = 21;
+       int valB = 6;
+       int valC = 3;
+       int valD = 1;
+
+       int result1 = valA - valB / valC;
+
+       int result2 = (valA - valB) / valC;
+
+       System.out.println(result1);
+       System.out.println(result2);
+
+       int result3 = valA / valC * valD + valB;
+       int result4 = valA / (valC * (valD + valB));
+
+       System.out.println(result3);
+       System.out.println(result4);
     }
+
 }
