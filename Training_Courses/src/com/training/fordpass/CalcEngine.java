07985d221e4ff9f2436908c46f3da9c39e0c01a0
ScottDonovan <spdonovan@hotmail.co.uk>
2022-01-18 17:35:47 +0000

    refactor to use switch statements

--- Training_Courses/src/com/training/fordpass/CalcEngine.java
+++ Training_Courses/src/com/training/fordpass/CalcEngine.java
@@ -8,19 +8,23 @@ public class CalcEngine {
         double result = 0.0d;
         char opCode = 'd';
 
-        if(opCode == 'a')
-            result = value1 + value2;
-        else if(opCode == 's')
-            result = value1 - value2;
-        else if(opCode == 'm')
-            result = value1 * value2;
-        else if(opCode == 'd') {
-            if (value2 != 0)
-            result = value1 / value2;
-        }
-        else {
-            System.out.println("Invalid opCode:" + opCode);
-            result = 0.0d;
+        switch (opCode) {
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value1 - value2;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            case 'd':
+                result = value2 != 0 ? value1 / value2 : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode:" + opCode);
+                result = 0.0d;
+                break;
         }
 
         System.out.println(result);
