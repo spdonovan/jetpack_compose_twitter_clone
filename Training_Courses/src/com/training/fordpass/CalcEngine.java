ebd30b03ae681d27c8d6d7bca8815ac33ff3e9b9
ScottDonovan <spdonovan@hotmail.co.uk>
2022-01-20 13:34:53 +0000

    Execute method split into MathEquation Class

--- Training_Courses/src/com/training/fordpass/CalcEngine.java
+++ Training_Courses/src/com/training/fordpass/CalcEngine.java
@@ -6,41 +6,65 @@ import java.util.Scanner;
 public class CalcEngine {
 
     public static void main(String[] args) {
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
-
-        if (args.length == 0) {
-            for (int i = 0; i < opCodes.length; i++) {
-                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-            }
-
-            for (double currentResult : results)
-                System.out.println(currentResult);
-        }   else if(args.length == 1 && args[0].equals("interactive"))
-            executeInteractively();
-        else if(args.length == 3)
-            handleCommandLine(args);
-        else
-            System.out.println("Please provide an operation code and 2 numeric values");
-            Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.nextLine();
-            String[] parts = userInput.split(" ");
-            performOperation(parts);
+        performCalculations();
     }
 
-    private static void performOperation(String[] parts) {
-        char opCode = opCodeFromString(parts[0]);
-        if (opCode == 'w')
-            handleWhen(parts);
-        else {
-            double leftVal = valueFromWord(parts[1]);
-            double rightVal = valueFromWord(parts[2]);
-            double result = execute(opCode, leftVal, rightVal);
-            displayResult(opCode, leftVal, rightVal, result);
+    public static void performCalculations() {
+
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0d, 'm');
+
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.println("result = " + equation.result);
         }
     }
+
+        private static MathEquation create(double leftVal, double rightVal, char opCode) {
+            MathEquation equation = new MathEquation();
+            equation.leftVal = leftVal;
+            equation.rightVal = rightVal;
+            equation.opCode = opCode;
+            return equation;
+        }
+
+
+
+//        if (args.length == 0) {
+//            for (int i = 0; i < opCodes.length; i++) {
+//                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+//            }
+//
+//            for (double currentResult : results)
+//                System.out.println(currentResult);
+//        }   else if(args.length == 1 && args[0].equals("interactive"))
+//            executeInteractively();
+//        else if(args.length == 3)
+//            handleCommandLine(args);
+//        else
+//            System.out.println("Please provide an operation code and 2 numeric values");
+//            Scanner scanner = new Scanner(System.in);
+//            String userInput = scanner.nextLine();
+//            String[] parts = userInput.split(" ");
+//            performOperation(parts);
+//    }
+
+
+
+//    private static void performOperation(String[] parts) {
+//        char opCode = opCodeFromString(parts[0]);
+//        if (opCode == 'w')
+//            handleWhen(parts);
+//        else {
+//            double leftVal = valueFromWord(parts[1]);
+//            double rightVal = valueFromWord(parts[2]);
+//            double result = execute(opCode, leftVal, rightVal);
+//            displayResult(opCode, leftVal, rightVal, result);
+//        }
+// }
 
     private static void handleWhen(String[] parts) {
         LocalDate startDate = LocalDate.parse(parts[1]);
@@ -52,16 +76,6 @@ public class CalcEngine {
 
     private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
         char symbol = symbolFromOpCode(opCode);
-//        StringBuilder builder = new StringBuilder(20);
-//        builder.append(leftVal);
-//        builder.append(" ");
-//        builder.append(symbol);
-//        builder.append(" ");
-//        builder.append(rightVal);
-//        builder.append(" = ");
-//        builder.append(result);
-//        String output = builder.toString();
-
         String output = String.format("%.3f %c %.3f = %.3f", leftVal, symbol, rightVal, result);
         System.out.println(output);
     }
@@ -83,36 +97,14 @@ public class CalcEngine {
         System.out.println("Enter an operation and 2 numbers:");
     }
 
-    private static void handleCommandLine(String[] args) {
-        char opCode = args[0].charAt(0);
-        double leftVal = Double.parseDouble(args[1]);
-        double rightVal = Double.parseDouble(args[2]);
-        double result = execute(opCode, leftVal, rightVal);
-        System.out.println(result);
-    }
+//    private static void handleCommandLine(String[] args) {
+//        char opCode = args[0].charAt(0);
+//        double leftVal = Double.parseDouble(args[1]);
+//        double rightVal = Double.parseDouble(args[2]);
+//        double result = execute(opCode, leftVal, rightVal);
+//        System.out.println(result);
+//    }
 
-    static double execute(char opCode, double leftVal, double rightVal) {
-        double result;
-        switch (opCode) {
-            case 'a':
-                result = leftVal + rightVal;
-                break;
-            case 's':
-                result = leftVal - rightVal;
-                break;
-            case 'm':
-                result = leftVal * rightVal;
-                break;
-            case 'd':
-                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
-                break;
-            default:
-                System.out.println("Invalid opCode:" + opCode);
-                result = 0.0d;
-                break;
-        }
-        return result;
-    }
 
     static char opCodeFromString(String operartionName) {
         char opCode = operartionName.charAt(0);
---
+++ Training_Courses/src/com/training/fordpass/MathEquation.java
@@ -0,0 +1,30 @@
+package com.training.fordpass;
+
+public class MathEquation {
+    double leftVal;
+    double rightVal;
+    char opCode;
+    double result;
+
+    void execute() {
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode:" + opCode);
+                result = 0.0d;
+                break;
+        }
+    }
+
+}
