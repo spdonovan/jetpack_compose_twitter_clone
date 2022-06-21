c188e42874764a1ad6f0d24e841abab27e43ce76
ScottDonovan <spdonovan@hotmail.co.uk>
2022-06-21 10:49:05 +0100

    Simple messagecard using compose

--- AndroidStudioProjects/JetPackTrials/app/src/main/java/com/example/jetpacktrials/MainActivity.kt
+++ AndroidStudioProjects/JetPackTrials/app/src/main/java/com/example/jetpacktrials/MainActivity.kt
@@ -4,12 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World!")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Scott", "Let's learn Jetpack Compose"))
 }
