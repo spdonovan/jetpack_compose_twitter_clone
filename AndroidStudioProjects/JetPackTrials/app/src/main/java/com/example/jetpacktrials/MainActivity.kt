f8b8fdd76cb548d190490a9fa1f64825bb6b3aba
ScottDonovan <spdonovan@hotmail.co.uk>
2022-06-21 12:05:02 +0100

    Added some layout constraints and profile picture

--- AndroidStudioProjects/JetPackTrials/app/src/main/java/com/example/jetpacktrials/MainActivity.kt
+++ AndroidStudioProjects/JetPackTrials/app/src/main/java/com/example/jetpacktrials/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
+import androidx.compose.material.Surface
 
 
 class MainActivity : ComponentActivity() {
@@ -34,14 +37,26 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         
         Spacer(modifier = Modifier.width(8.dp))
 
     Column {
-        Text(text = msg.author)
+        Text(
+            text = msg.author,
+            color = MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2
+        )
         Spacer(modifier = Modifier.height(4.dp))
-        Text(text = msg.body)
+
+        Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Text(
+                text = msg.body,
+                modifier = Modifier.padding(all = 4.dp),
+                style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
